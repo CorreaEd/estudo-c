fed08343054c3751f217132758a305d6ee1f56aa
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-04-08 09:57:40 -0300

    add rand e srand + função time
    Foi adicionada a função rand e srand + package time.h para que o "número secreto" seja sempre randômico.

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-
     printf ("*************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhação *\n");
     printf("**************************************\n");
 
-    int numerosecreto = 42;
+    int segundos = time(0);
+    srand(segundos);
 
+    int numerogrande = rand();
+
+    int numerosecreto = numerogrande % 100;
     int chute;
-   
     int tentativas = 1;
-   
     double pontos = 1000;
-
-
 
     while(1) {
 
@@ -27,34 +27,27 @@ int main() {
 
         if(chute < 0) {
             printf("Você não pode chutar números negativos!\n");
-
             continue;
         }
-        
         int acertou = (chute == numerosecreto);
         int maior = chute > numerosecreto;
 
         if(acertou) {
             printf("Parabéns! Você acertou!\n");
             printf("Você é um bom adivinha!\n\n");
-            
             break;
         }
         else if(maior) {
             printf("Seu chute foi maior que o número secreto!\n\n");
         } 
-
         else {
                 printf("Seu chute foi menor que o número secreto!\n\n");
         }
-
         tentativas++;
 
         double pontosperdidos = abs(chute - numerosecreto) / (double)2;
         pontos = pontos - pontosperdidos;
-        
     }
-
     printf("Fim de jogo!");
     printf("Você acertou em %d tentativas!\n", tentativas);
     printf("Total de pontos: %.1f\n", pontos);
